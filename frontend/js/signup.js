661bbd212fe83038c87d82b33627305d0cb90e53
Gellért <gellertkecskes@gmail.com>
2026-07-10 13:59:04 +0200

    rerange the Folder and file system and add auth fucntion to the .js

---
+++ frontend/js/signup.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function () {
+    const signupForm = document.querySelector('section');
+    signupForm.style.opacity = 0;
+    setTimeout(() => {
+        signupForm.style.transition = 'opacity 1s ease-in-out';
+        signupForm.style.opacity= 1;
+    },500);
+
+    const signupButton= document.querySelector("button");
+    signupButton.addEventListener("click", function () {
+        const emailInput = document.querySelector('input[type="email"]');
+        const passwordInput = document.querySelector('input [type="password"}');
+        const confirmPasswordInput = document.querySelector('input [type="password"] [name="confirm-password"]');
+
+        //check for a valid email and password
+        const valid  = emailInput.checkValidity() && passwordInput.checkValidity() && confirmPasswordInput.checkValidity();
+        if (!valid) {
+            signupForm.classList.add('shake');
+            setTimeout(() => {
+                signupForm.classList.remove('shake');
+            }, 1000);
+        }
+    })
+
+
+
+
+});
