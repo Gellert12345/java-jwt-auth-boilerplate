4025bc885cf4353151be88ba5874da617218d193
Gellért <gellertkecskes@gmail.com>
2026-07-10 14:52:19 +0200

    wrting the API endponintsű

---
+++ backend/src/main/java/com/Controller/ContentController.java
@@ -0,0 +1,18 @@
+package com.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // handle incoming input from users
+public class ContentController {
+    @GetMapping("/login") // login.html
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("req/signup") // req mert adatokat kérek a frontedntől a backendbe (getmapping => figyeld és
+                              // hallgassd ez. aporotot)
+    public String signup() {
+        return "";
+    }
+}
