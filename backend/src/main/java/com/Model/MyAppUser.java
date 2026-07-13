cd535ffd83e03de58a5558daeb62f69b05315585
Gellért <gellertkecskes@gmail.com>
2026-07-13 13:48:55 +0200

    database schema like json data format configure in java

--- backend/src/main/java/com/Model/MyAppUser.java
+++ backend/src/main/java/com/Model/MyAppUser.java
@@ -10,7 +10,40 @@ import jakarta.persistence.Id;
 @Getter // hogy ki tudjam ovlasni az adatokat
 @Entity // csinalj egy táblát az adatbázisba
 public class MyAppUser {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id // primary key (közvelten az alatta lévő dekralált valtozóra lesz érvényes!)
+    @GeneratedValue(strategy = GenerationType.AUTO) // mindegyikhez hozzá rendel automatikus egy szamot
+                                                    // sorrenbe(1.2.3.4)
     private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    // getter and setter shiiiiiittsss
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
