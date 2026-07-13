dd7b3820dbca883fb1c5841299a08396388882a9
Gellért <gellertkecskes@gmail.com>
2026-07-13 13:29:55 +0200

    generate schema for the Postgresql DB

--- backend/src/main/java/com/Model/MyAppUser.java
+++ backend/src/main/java/com/Model/MyAppUser.java
@@ -1,8 +1,16 @@
+//connect to DB file(and maybe store data)
 package com.Model;
 
-//connect to DB file(and maybe store data)
 import lombok.Getter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Getter // hogy ki tudjam ovlasni az adatokat
+@Entity // csinalj egy táblát az adatbázisba
 public class MyAppUser {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
