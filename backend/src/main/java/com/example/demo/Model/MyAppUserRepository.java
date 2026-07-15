5e0117067f82be4331c7f6c4b3705bc47e1789c9
Gellért <gellertkecskes@gmail.com>
2026-07-15 23:51:56 +0200

    ice n go summer job after push

--- backend/src/main/java/com/example/demo/Model/MyAppUserRepository.java
+++ backend/src/main/java/com/example/demo/Model/MyAppUserRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.Model;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Repositroy to interact with the database!! komunikávios réteg
 public interface MyAppUserRepository extends JpaRepository<MyAppUser, Long> {
-
+    Optional<MyAppUser> findByUsername(String username); // Optional => return a data or it tells or its empty and dont
+                                                         // thow errors
 }
---
+++ backend/src/main/java/com/example/demo/Model/myAppUserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.Model;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class myAppUserService {
+
+}
